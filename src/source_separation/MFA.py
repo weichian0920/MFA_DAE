61bd6d45ea0ec4c914a97a6fa493a13a7ac5de61
weichian0920 <weichian0920@gmail.com>
2020-12-12 19:18:55 +0800

    Add files via upload

--- src/source_separation/MFA.py
+++ src/source_separation/MFA.py
@@ -1,6 +1,8 @@
-#/--coding:utf-8/
-#/author:Ethan Wang/
+# -*- coding: utf-8 -*-
+#Author:Wei-Chien Wang
 
+import sys
+sys.path.append('../')
 import torch
 import numpy as np
 import librosa
@@ -11,30 +13,36 @@ import scipy.io.wavfile as wav
 from utils.signalprocess import lps2wav
 
 class MFA_source_separation(object):
-    """MFA analysis for unsupervised monoaural source separation.
-    this function separate different sources in unsupervised manner depend on their periodicity properties.
-    # Arguments
-      source number: indicate separated source number.
-      clustering _alg: clustering algorithm for MFA analysis.
-      wienner_mask: the output would be mask by constructed ratio mask.
+
     """
-    def __init__(self, model=None, args=None):
+    MFA analysis for unsupervised monoaural source separation.
+        This function separate different sources by unsupervised manner depend on different source's periodicity properties.
+    Arguments:
+        model: deep autoencoder for source separation.
+        source number: separated source number.
+        clustering _alg: clustering algorithm for MFA analysis.
+        wienner_mask: if True the output is mask by constructed ratio mask.
+        FFT_dict: fouier transform parameters.
+    """
+
+    def __init__(self, model=None, FFT_dict=None, args=None):
 
         self.model = model
-        self.clustering_alg = args.clustering_alg
         self.source_num = args.source_num
+        self.clustering_alg = args.clustering_alg
         self.wienner_mask = args.wienner_mask
+        self.FFT_dict = FFT_dict
+        self.args = args
 
     def FFT_(self, input):
 
         epsilon = np.finfo(float).eps
-
         frame_num = input.shape[1]
         encoding_shape = input.shape[0]
-    
         FFT_result = np.zeros((encoding_shape, int(frame_num/2+1)))
+
         for i in range(0, encoding_shape):
-            fft_r = librosa.stft(input[i,:], n_fft=frame_num, hop_length = frame_num+1, window = scipy.signal.hamming)
+            fft_r = librosa.stft(input[i,:], n_fft=frame_num, hop_length=frame_num+1, window=scipy.signal.hamming)
             fft_r = fft_r+ epsilon
             FFT_r = abs(fft_r)**2
             FFT_result[i] = np.reshape(FFT_r, (-1,))
@@ -42,13 +50,16 @@ class MFA_source_separation(object):
         return FFT_result
     
     def freq_modulation(self, source_idx, label, encoded_img):
-
-        #Encoded_img : latent coded matrix
-        #latent label : latent neuron label
-        #min_value : minimun value of latent unit which use to deactivate neuron
-        frame_num = encoded_img.shape[0]# encoded_img = (encoding_shape, frame_num)
-        encoding_shape = encoded_img.shape[1]# encoded_img = (encoding_shape, frame_num)
-        min_value = torch.min(encoded_img)
+        """
+          This function use to modulate latent code.
+          Arguments:
+            source_idx: source no. 
+            Encoded_img: latent coded matrix. Each dimension represnet as (encoding_shape, frame_num)
+            label: latent neuron label.
+        """
+        frame_num = encoded_img.shape[0]
+        encoding_shape = encoded_img.shape[1]
+        min_value = torch.min(encoded_img)#minimun value of latent unit which use to deactivate neuron.
 
         for k in range(0, encoding_shape):
             if(label[k] != source_idx):
@@ -58,10 +69,11 @@ class MFA_source_separation(object):
 
     def MFA(self, input, source_num = 2):
         """
-        The input x dim represent is (frame number, encoded neuron's number)
+          This function attempt to find each latent neuron is activated by which source by clustering(K_MEANS of sparsity NMF clustering).
+          Note: Each dimension of input represent as (frame number, encoded neuron's number).
         """
         encoded_dim  = input.shape[1]
-        ###############period clustering
+        #Period clustering
         fft_bottleneck = self.FFT_(input.T)#(fft(encoded, frame_num))
 
         if self.clustering_alg=="K_MEAMS":
@@ -71,33 +83,46 @@ class MFA_source_separation(object):
 
         return k_labels
 
-    def source_separation(self, input, phase, mean = 0, std = 1, filedir="../test/result/", filename="test"):
+    def source_separation(self, input, phase, mean, std, filedir="../test_/result/", filename="test"):
+        """
+          main function for blind source separation.
+        """
+ 
+        feature_dim = self.FFT_dict['frequency_bins'][1]-self.FFT_dict['frequency_bins'][0]
 
-        input = torch.tensor(input).cuda()
-        sources = torch.unsqueeze(torch.zeros_like(input), 0)
-        latent_code = self.model.encoder(input)
+        if self.args.model_type == "DAE_C":
+            x = np.reshape((input.T), (-1,1,1,int(self.FFT_dict['FFTSize']/2+1)))[:,:,:,self.FFT_dict['frequency_bins'][0]:self.FFT_dict['frequency_bins'][1]]
+        else:
+            x = input.T[:,self.FFT_dict['frequency_bins'][0]:self.FFT_dict['frequency_bins'][1]]
+        x = torch.tensor(x).cuda()
+        sources = torch.unsqueeze(torch.zeros_like(x), 0)
+        #Encode input
+        latent_code = self.model.encoder(x)
         #MFA analysis for identifying latent neurons 's label
         label = self.MFA(latent_code.cpu().detach().numpy())
-        #reconstruct input
+        #Reconstruct input
         sources[0] = self.model.decoder(latent_code)
-        #generate the latent code and decode the separated latent code
+        #Discriminate latent code for different sources.
         for source_idx in range(0, self.source_num):
             y_s = self.freq_modulation(source_idx, label, latent_code)
             sources = torch.cat((sources, torch.unsqueeze(self.model.decoder(y_s), 0)), 0)
         sources = torch.squeeze(sources).permute(0,2,1).detach().cpu().numpy()
-        
-        #
+        #Source separation
         for source_idx in range(0, self.source_num+1):
+            sources[source_idx,:,:] = np.sqrt(10**((sources[source_idx,:,:]*std[self.FFT_dict['frequency_bins'][0]:self.FFT_dict['frequency_bins'][1],:])+mean[self.FFT_dict['frequency_bins'][0]:self.FFT_dict['frequency_bins'][1],:]))
+
+        input = np.sqrt(10**(input*std+mean))
+        for source_idx in range(0, self.source_num+1):
+            Result = np.array(input)
             if(self.wienner_mask==True):
-                if source_idx==0:#reconstruct original signal
-                    Result = sources[0,:,:]*std+mean
+                if source_idx == 0:#Reconstruct original signal
+                    Result[self.FFT_dict['frequency_bins'][0]:self.FFT_dict['frequency_bins'][1],:] = sources[0,:,:]
                 else:
-                    Result = ((sources[source_idx,:,:]*std+mean)/(np.sum(sources[1:,:,:], axis = 0)*std+mean)*sources[0,:,:]*std+mean)
-            else:#wienner_mask==False
-                Result = np.array(sources[source_idx,:,:])*std+mean
-            Result = np.sqrt(10**(Result))
+                    Result[self.FFT_dict['frequency_bins'][0]:self.FFT_dict['frequency_bins'][1],:] =  2*(sources[source_idx,:,:]/(np.sum(sources[1:,:,:], axis = 0)))*sources[0,:,:]
+            else:#Wienner_mask==False
+                Result[self.FFT_dict['frequency_bins'][0]:self.FFT_dict['frequency_bins'][1],:] = np.array(sources[source_idx,:,:])
             R = np.multiply(Result, phase)
-            result = librosa.istft(R, hop_length = 256, win_length = 512, window=scipy.signal.hamming, center = False)
+            result = librosa.istft(R, hop_length=self.FFT_dict['Hop_length'], win_length=self.FFT_dict['Win_length'], window=scipy.signal.hamming, center=False)
             result = np.int16(result*32768)
             if source_idx==0:
                 result_path = "{0}reconstruct/".format(filedir)
@@ -105,6 +130,4 @@ class MFA_source_separation(object):
                 result_path = "{0}source{1}/".format(filedir, source_idx)
             if not os.path.exists(result_path):
                 os.makedirs(result_path)
-            wav.write("{0}{1}.wav".format(result_path,  filename), 8000, result)
-
-    
+            wav.write("{0}{1}.wav".format(result_path,  filename), self.FFT_dict['sr'], result)
